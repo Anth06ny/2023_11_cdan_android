34419527ce0f904fadc25940aaf19a8b932f921c
AnthonyPCPortable <anth06ny@gmail.com>
2023-11-22 14:09:21 +0100

    Correction Compose partie 2

--- app/src/main/java/com/example/a2023_11_cdan_android/MainActivity.kt
+++ app/src/main/java/com/example/a2023_11_cdan_android/MainActivity.kt
@@ -3,19 +3,12 @@ package com.example.a2023_11_cdan_android
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.tooling.preview.Preview
-import com.example.a2023_11_cdan_android.exo.main
-import com.example.a2023_11_cdan_android.exo.pictureList
-import com.example.a2023_11_cdan_android.ui.screens.PictureRowItem
+import com.example.a2023_11_cdan_android.ui.screens.SearchScreen
 import com.example.a2023_11_cdan_android.ui.theme._2023_11_cdan_androidTheme
 
 class MainActivity : ComponentActivity() {
@@ -24,35 +17,10 @@ class MainActivity : ComponentActivity() {
         setContent {
             _2023_11_cdan_androidTheme {
                 // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background) {
-                    Column {
-                        PictureRowItem(pictureList[0], modifier = Modifier.background(Color.Yellow))
-                    }
+                Surface(modifier = Modifier.fillMaxSize(), color = Color.LightGray) {
+                    SearchScreen()
                 }
-
             }
         }
-    }
-}
-
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier,
-    )
-}
-
-@Preview(showBackground = true,
-    showSystemUi = true
-
-)
-@Composable
-fun GreetingPreview() {
-    _2023_11_cdan_androidTheme {
-        Greeting("Android")
     }
 }
--- app/src/main/java/com/example/a2023_11_cdan_android/ui/screens/SearchScreen.kt
+++ app/src/main/java/com/example/a2023_11_cdan_android/ui/screens/SearchScreen.kt
@@ -1,13 +1,29 @@
 package com.example.a2023_11_cdan_android.ui.screens
 
+import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.heightIn
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.widthIn
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Clear
+import androidx.compose.material.icons.filled.Search
+import androidx.compose.material.icons.filled.Send
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
@@ -23,20 +39,101 @@ import com.example.a2023_11_cdan_android.exo.PictureData
 import com.example.a2023_11_cdan_android.exo.pictureList
 import com.example.a2023_11_cdan_android.ui.theme._2023_11_cdan_androidTheme
 
-@Preview(showBackground = true, showSystemUi = true)
+//Affichage de la preview de l'écran
+@Preview(
+    showBackground = true,
+    showSystemUi = true
+)
 @Composable
-fun PictureRowItemPreview() {
-    //nom visible dans MainActivity en dessous de setContent{
+fun SearchScreenPreview() {
     _2023_11_cdan_androidTheme {
-        Column {
-            PictureRowItem(pictureList[0])
+        Surface(modifier = Modifier.fillMaxWidth(), color = Color.LightGray) {
+            SearchScreen()
         }
     }
 }
 
+//Composable représentant l'ensemble de l'écran
+@OptIn(ExperimentalFoundationApi::class)
+@Composable
+fun SearchScreen(modifier: Modifier = Modifier) {
+    Column(modifier) {
+        SearchBar()
+        Spacer(Modifier.size(8.dp))
+        LazyColumn(
+            verticalArrangement = Arrangement.spacedBy(8.dp),
+            modifier = Modifier.weight(1f)
+
+        ) {
+            items(pictureList.size) {
+                PictureRowItem(pictureList[it], modifier = Modifier.background(Color.White))
+            }
+        }
+
+        Row(
+
+            horizontalArrangement = Arrangement.Center,
+            modifier = Modifier.fillMaxWidth()
+        ) {
+            Button(
+                onClick = { /* Do something! */ },
+                contentPadding = ButtonDefaults.ButtonWithIconContentPadding
+            ) {
+                Icon(
+                    Icons.Filled.Clear,
+                    contentDescription = "Localized description",
+                    modifier = Modifier.size(ButtonDefaults.IconSize)
+                )
+                Spacer(Modifier.size(ButtonDefaults.IconSpacing))
+                Text("Clear Filter")
+            }
+
+            Button(
+                onClick = { /* Do something! */ },
+                contentPadding = ButtonDefaults.ButtonWithIconContentPadding
+            ) {
+                Icon(
+                    Icons.Filled.Send,
+                    contentDescription = "Localized description",
+                    modifier = Modifier.size(ButtonDefaults.IconSize)
+                )
+                Spacer(Modifier.size(ButtonDefaults.IconSpacing))
+                Text("Load Data")
+            }
+        }
+
+
+        //Bouton x2
+    }
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun SearchBar(modifier: Modifier = Modifier) {
+    TextField(
+        value = "", //Valeur par défaut
+        onValueChange = { newValue -> }, //Action
+        leadingIcon = { //Image d'icone
+            Icon(
+                imageVector = Icons.Default.Search,
+                tint = MaterialTheme.colorScheme.primary,
+                contentDescription = null
+            )
+        },
+        label = { Text("Enter text") }, //Texte d'aide qui se déplace
+
+        //Comment le composant doit se placer
+        modifier = modifier
+            .fillMaxWidth() // Prend toute la largeur
+            .heightIn(min = 56.dp) //Hauteur minimum
+    )
+}
+
+//Composable affichant 1 PictureData
 @OptIn(ExperimentalGlideComposeApi::class)
 @Composable
 fun PictureRowItem(data: PictureData, modifier: Modifier = Modifier) {
+
     Row(modifier = modifier.height(100.dp)) {
 
         GlideImage(
@@ -62,8 +159,7 @@ fun PictureRowItem(data: PictureData, modifier: Modifier = Modifier) {
             Text(
                 text = data.longText.take(15),
                 fontSize = 14.sp,
-
-                )
+            )
         }
     }
 }
